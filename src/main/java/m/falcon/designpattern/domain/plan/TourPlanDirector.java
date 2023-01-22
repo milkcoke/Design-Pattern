4a2c62fcda603dfa39695878ca50f85a01130b43
milckoke <mbh023@gmail.com>
2023-01-22 22:53:43 +0900

    feat: Add Director class for wrapping making specific instance
    Director is used to wrapping or templating the specific instance type on business.

---
+++ src/main/java/m/falcon/designpattern/domain/plan/TourPlanDirector.java
@@ -0,0 +1,30 @@
+package m.falcon.designpattern.domain.plan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class TourPlanDirector {
+    private final TourPlanBuilder tourPlanBuilder;
+
+    @Autowired
+    public TourPlanDirector(TourPlanBuilder tourPlanBuilder) {
+        this.tourPlanBuilder = tourPlanBuilder;
+    }
+
+    public TourPlan createHawaiiPackageTourPlan(LocalDate startDate, LocalDate endDate) {
+        return this.tourPlanBuilder
+                .title("Hawaii")
+                .date(startDate, endDate)
+                .build();
+    }
+
+    public TourPlan createClimbingPlan(LocalDate startDate, LocalDate endDate) {
+        return this.tourPlanBuilder
+                .title("동네뒷산")
+                .date(startDate, endDate)
+                .build();
+    }
+}
