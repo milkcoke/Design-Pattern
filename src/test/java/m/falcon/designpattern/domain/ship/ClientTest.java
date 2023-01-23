ada56b9ccc01982e27bf43199cf60ae60885bfb4
milckoke <mbh023@gmail.com>
2023-01-23 23:42:12 +0900

    chore: Update typo

--- src/test/java/m/falcon/designpattern/domain/ship/ClientTest.java
+++ src/test/java/m/falcon/designpattern/domain/ship/ClientTest.java
@@ -9,11 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class ClientTest {
     private Client client = new Client("카르마", "karma@gmail.com");
 
-    @DisplayName("클라이언트 메소드를 통한 생성자 의존성 주입")
+    @DisplayName("클라이언트 메소드를 통한 생성자 의존성 주입 테스트")
     @Test
     void clientOrderDiTest() {
+        Ship blackOrderedShip = client.orderShip(new BlackShipFactory(), "black-ship");
+        Ship blackship = new BlackShipFactory().createShip();
+        assertThat(blackOrderedShip.getColor()).isEqualTo("black");
+        assertThat(blackship.getClass().getSimpleName()).isEqualTo("BlackShip");
+    }
+
+    @DisplayName("클라이언트 주문 실패 테스트")
+    @Test
+    void clientOrderFailTest() {
         assertThrows(IllegalArgumentException.class, ()->client.orderShip(new BlackShipFactory(), "blackship"));
-        Ship blackShip = client.orderShip(new BlackShipFactory(), "black-ship");
-        assertThat(blackShip.getColor()).isEqualTo("black");
     }
 }
