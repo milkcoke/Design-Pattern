1e4a5586267b75efd58319a85b1c4ac151b55801
milckoke <mbh023@gmail.com>
2023-01-24 22:23:52 +0900

    feat: Define CommentDecorator
    Apply decorator pattern.

---
+++ src/main/java/m/falcon/designpattern/domain/comment/service/TrimmingCommentDecorator.java
@@ -0,0 +1,15 @@
+package m.falcon.designpattern.domain.comment.service;
+
+public class TrimmingCommentDecorator extends CommentDecorator {
+    public TrimmingCommentDecorator(CommentService commentService) {
+        super(commentService);
+    }
+    @Override
+    public void addComment(String comment) {
+        super.addComment(trimComment(comment));
+    }
+
+    private String trimComment(String comment) {
+        return comment.replace("..", "");
+    }
+}
