b14b48146620728a95f82f8052e2f2ff308f1fbf
milckoke <mbh023@gmail.com>
2023-01-26 20:39:18 +0900

    feat: Define DefaultGameService implementation

---
+++ src/main/java/m/falcon/designpattern/domain/game/DefaultGameService.java
@@ -0,0 +1,12 @@
+package m.falcon.designpattern.domain.game;
+
+public class DefaultGameService implements GameService {
+    public void loadGame() throws InterruptedException {
+        System.out.println("게임 로딩중입니다.");
+        Thread.sleep(1000L);
+        System.out.println("로딩 완료");
+    }
+    public void startGame() {
+        System.out.println("게임 시작!");
+    }
+}
