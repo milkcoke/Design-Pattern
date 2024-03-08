4c5e1bed20a95e7a087dd6941ac582e64277a689
milckoke <mbh023@gmail.com>
2024-03-09 00:49:57 +0900

    feat: Add an enum singleton class

---
+++ src/main/java/m/falcon/designpattern/createpattern/singleton/ConstantSettings.java
@@ -0,0 +1,10 @@
+package m.falcon.designpattern.createpattern.singleton;
+
+/**
+ * Thread Safe but Eager Loading
+ * Prevent from breaking singleton by reflection
+ */
+enum ConstantSettings {
+  INSTANCE;
+
+}
