0a2ba5740171c8699d494088152d7f99d1ce8452
milckoke <mbh023@gmail.com>
2025-03-08 14:44:21 +0900

    test: Beverage 통합테스트 추가

---
+++ src/test/java/pattern/behavior/decorator/v1/BeverageTest.java
@@ -0,0 +1,29 @@
+package pattern.behavior.decorator.v1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pattern.behavior.decorator.v1.beverage.Espresso;
+import pattern.behavior.decorator.v1.decorator.Mocha;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BeverageTest {
+
+  @DisplayName("커피 주문 통합 테스트")
+  @Test
+  void starbuzzTest() {
+    // given
+    Beverage beverage = new Espresso();
+    assertThat(beverage.cost()).isEqualTo(1.99);
+    assertThat(beverage.getDescription()).isEqualTo("에스프레소");
+
+    Beverage mochaEspresso = new Mocha(beverage);
+    assertThat(mochaEspresso.cost()).isEqualTo(2.19);
+    assertThat(mochaEspresso.getDescription()).isEqualTo("에스프레소, 모카");
+
+    Beverage doubleMochaEspresso = new Mocha(mochaEspresso);
+    assertThat(doubleMochaEspresso.cost()).isEqualTo(2.39);
+    assertThat(doubleMochaEspresso.getDescription()).isEqualTo("에스프레소, 모카, 모카");
+  }
+
+}
