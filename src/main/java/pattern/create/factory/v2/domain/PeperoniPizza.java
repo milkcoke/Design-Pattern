0ad69c676f3d26656bf5249e3825baeff973a217
milkcoke <mbh023@gmail.com>
2025-03-12 13:03:59 +0900

    feat: Define pizza and concrete classes
    - Those are domain classes for expressing simple factory pattern

---
+++ src/main/java/pattern/create/factory/v2/domain/PeperoniPizza.java
@@ -0,0 +1,4 @@
+package pattern.create.factory.v2.domain;
+
+public record PeperoniPizza() implements Pizza {
+}
---
+++ src/main/java/pattern/create/factory/v2/domain/VeggiePizza.java
@@ -0,0 +1,4 @@
+package pattern.create.factory.v2.domain;
+
+public record VeggiePizza() implements Pizza {
+}
